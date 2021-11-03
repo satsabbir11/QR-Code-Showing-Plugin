398a76418fec112635244d0d04ce29f464f44725
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2021-11-03 10:58:28 +0600

    add custion setting to general to get dimension of qr code

--- index.php
+++ index.php
@@ -24,11 +24,37 @@ function qrcode_genarator($content){
     if(in_array($current_post_type,$excluded_post_type)){
         return $content;
     }
-    $dimension = apply_filters("qrcode_dimension","100x100");
+
+    $width = get_option("qrcode_width");
+    $height = get_option("qrcode_height");
+
+    $width = $width?$width:100;
+    $height = $height?$height:100;
+
+    $dimension = apply_filters("qrcode_dimension","{$width}x{$height}");
     $image_attribute = apply_filters("qrcode_attribute","");
     $image_url = sprintf("https://api.qrserver.com/v1/create-qr-code/?size=%s&data=%s",$dimension,$post_url);
     $content .=sprintf("<div class='qrcode2'><img %s src='%s' alt='%s'/></div>",$image_attribute,$image_url,$image_alt);
     return $content;
 }
 add_filter("the_content","qrcode_genarator");
+
+function qrcode_setting_init(){
+    add_settings_field("qrcode_width",__("QR Code Width","qrcode"),"qrcode_width_setting","general");
+    add_settings_field("qrcode_height",__("QR Code Height","qrcode"),"qrcode_height_setting","general");
+
+    register_setting("general","qrcode_width",array("sanitize_callback"=>"esc_attr"));
+    register_setting("general","qrcode_height",array("sanitize_callback"=>"esc_attr"));
+}
+
+function qrcode_width_setting(){
+    $width = get_option("qrcode_width");
+    printf("<input type='text' id='%s' name='%s' value='%s'></input>","qrcode_width","qrcode_width",$width);
+}
+function qrcode_height_setting(){
+    $height = get_option("qrcode_height");
+    printf("<input type='text' id='%s' name='%s' value='%s'></input>","qrcode_height","qrcode_height",$height);
+}
+add_action("admin_init","qrcode_setting_init");
+
 
